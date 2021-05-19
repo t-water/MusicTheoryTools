c380a3cc4c070a1bd47124783b54bd127ea3c962
Tyler Waterman <watermant@hhmi.org>
2021-05-19 18:17:51 -0400

    Initial Commit

---
+++ src/components/IntervalTraining.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface IIntervalTrainingProps{
+
+}
+
+const IntervalTraining = (props: IIntervalTrainingProps) => { 
+  return (
+    <></>
+  )
+}
+
+export default IntervalTraining;
