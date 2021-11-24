9d14d6835bf0c4c6a7c1a5e6bb4294caafc9a01d
Tyler Waterman <watermant@hhmi.org>
2021-11-24 13:05:57 -0500

    First attempt at generating notes for ear training

--- src/components/EarTraining.tsx
+++ src/components/EarTraining.tsx
@@ -1,27 +1,53 @@
 import React from 'react';
-import { INoteSound, noteSounds } from '../types/NoteSounds';
+import { SEMI_TONES_IN_AN_OCTAVE } from '../types/Constants';
+import { INoteSound, MAX_DISTANCE_FROM_MIDDLE_C, MIN_DISTANCE_FROM_MIDDLE_C, noteSoundsArr } from '../types/NoteSounds';
 
 interface IEarTrainingProps {
-
 }
 
 const EarTraining = (props: IEarTrainingProps) => { 
-    const noteSoundsPropertyNames: string[] = Object.getOwnPropertyNames(noteSounds);
-    const numberOfSounds: number = noteSoundsPropertyNames.length;
-    const randomNoteSoundsPropertyName: string = noteSoundsPropertyNames[Math.floor(Math.random() * numberOfSounds)];
-    const randomNoteAudio: INoteSound = (noteSounds as any)[randomNoteSoundsPropertyName];
-    
-    const noteAudio: JSX.Element =  (
+    const maxDistance: number = MAX_DISTANCE_FROM_MIDDLE_C-1;
+    const minDistance: number = MIN_DISTANCE_FROM_MIDDLE_C+1;
+    const randomNoteDistance: number = Math.floor(Math.random() * (maxDistance - minDistance)) + minDistance;
+
+    const firstNoteSound: INoteSound = noteSoundsArr.filter((noteSound: INoteSound) => noteSound.DistanceFromMiddleC === randomNoteDistance)[0];
+
+    const possibleNoteSounds: INoteSound[] = noteSoundsArr.filter((noteSound: INoteSound) => {
+        const noteIsHigherThanFirst: boolean = noteSound.DistanceFromMiddleC > firstNoteSound.DistanceFromMiddleC;
+        const noteIsWithinOneOctave: boolean = noteSound.DistanceFromMiddleC <= Math.min(randomNoteDistance + SEMI_TONES_IN_AN_OCTAVE, maxDistance+1);
+
+        return noteIsHigherThanFirst && noteIsWithinOneOctave;
+    });
+
+    const secondNoteSoundsIndex: number = Math.floor(Math.random() * possibleNoteSounds.length);
+
+    const secondNoteSound: INoteSound = possibleNoteSounds[secondNoteSoundsIndex];
+
+    const firstNoteAudio: JSX.Element =  (
         <audio controls>
-            <source src = {`https://docs.google.com/uc?export=download&id=${randomNoteAudio.GoogleDriveId}`} type = 'audio/wav'/>
+            <source src = {`https://docs.google.com/uc?export=download&id=${firstNoteSound.GoogleDriveId}`} type = 'audio/wav'/>
+        </audio>
+    )
+    
+    const secondNoteAudio: JSX.Element =  (
+        <audio controls>
+            <source src = {`https://docs.google.com/uc?export=download&id=${secondNoteSound.GoogleDriveId}`} type = 'audio/wav'/>
         </audio>
     )
 
     return (
         <>
-            <span>{randomNoteAudio.Note.Abbreviation}</span>
+            <div>
+                <span>{firstNoteSound.Note.Abbreviation}</span>
 
-            { noteAudio }
+                { firstNoteAudio }
+            </div>
+
+            <div>
+                <span>{secondNoteSound.Note.Abbreviation}</span>
+
+                { secondNoteAudio }
+            </div>
         </>
     )
 }
--- src/types/NoteSounds.ts
+++ src/types/NoteSounds.ts
@@ -6,6 +6,7 @@ export interface INoteSound {
     Octave: number;
     Note: Note;
     GoogleDriveId: string;
+    DistanceFromMiddleC: number;
 }
 
 export interface INoteSounds {
@@ -27,61 +28,80 @@ export const noteSounds: INoteSounds = {
     C3: {
         Octave: 3,
         Note: C,
-        GoogleDriveId: '1BpkFD9_749hMLSlWuRR8XL8aLB4GAaqy'
+        GoogleDriveId: '1BpkFD9_749hMLSlWuRR8XL8aLB4GAaqy',
+        DistanceFromMiddleC: -12
     },
     CSharp3: {
         Octave: 3,
         Note: CSharp,
-        GoogleDriveId: '1CQUsrtuWqOeRcoQVaAcWNs9t-smHDC3U'
+        GoogleDriveId: '1CQUsrtuWqOeRcoQVaAcWNs9t-smHDC3U',
+        DistanceFromMiddleC: -11
     },
     D3: {
         Octave: 3,
         Note: D,
-        GoogleDriveId: '1ioTfbHzhxKfjNuSmdqWRAmn05IhS9nyj'
+        GoogleDriveId: '1ioTfbHzhxKfjNuSmdqWRAmn05IhS9nyj',
+        DistanceFromMiddleC: -10
     },
     DSharp3: {
         Octave: 3,
         Note: DSharp,
-        GoogleDriveId: '16afZ6F8scJquISDGEUEduaeDCLnUlYNF'
+        GoogleDriveId: '16afZ6F8scJquISDGEUEduaeDCLnUlYNF',
+        DistanceFromMiddleC: -9
     },
     E3: {
         Octave: 3,
         Note: E,
-        GoogleDriveId: '1IWYCZgEGJrUNbo55VRJc2WMxkrkBOKAE'
+        GoogleDriveId: '1IWYCZgEGJrUNbo55VRJc2WMxkrkBOKAE',
+        DistanceFromMiddleC: -8
     },
     F3: {
         Octave: 3,
         Note: F,
-        GoogleDriveId: '1sT24CrIKIThc_KiQKWL5_hvv1uU7G_8l'
+        GoogleDriveId: '1sT24CrIKIThc_KiQKWL5_hvv1uU7G_8l',
+        DistanceFromMiddleC: -7
     },
     FSharp3: {
         Octave: 3,
         Note: FSharp,
-        GoogleDriveId: '19PFSsIKo8scHE7a65_3PJ7pcujiqtgcY'
+        GoogleDriveId: '19PFSsIKo8scHE7a65_3PJ7pcujiqtgcY',
+        DistanceFromMiddleC: -6
     },
     G3: {
         Octave: 3,
         Note: G,
-        GoogleDriveId: '1JNWSi0S4R_CTeEyQaPtiHqQuNIUOorwZ'
+        GoogleDriveId: '1JNWSi0S4R_CTeEyQaPtiHqQuNIUOorwZ',
+        DistanceFromMiddleC: -5
     },
     GSharp3: {
         Octave: 3,
         Note: GSharp,
-        GoogleDriveId: '1jTpVrMiE6t2ZWiofA4MXw-_JfKWNy5GG'
+        GoogleDriveId: '1jTpVrMiE6t2ZWiofA4MXw-_JfKWNy5GG',
+        DistanceFromMiddleC: -4
     },
     A3: {
         Octave: 3,
         Note: A,
-        GoogleDriveId: '1nqlk2lYSimFqlKGhjHnF2FiWJklYcE8q'
+        GoogleDriveId: '1nqlk2lYSimFqlKGhjHnF2FiWJklYcE8q',
+        DistanceFromMiddleC: -3
     },
     ASharp3: {
         Octave: 3,
         Note: ASharp,
-        GoogleDriveId: '1sOD43duOhbCl-kV7y7vWSjFTlXRbsBqY'
+        GoogleDriveId: '1sOD43duOhbCl-kV7y7vWSjFTlXRbsBqY',
+        DistanceFromMiddleC: -2
     },
     B3: {
         Octave: 3,
         Note: B,
-        GoogleDriveId: '1KZpfS0_k0zuF1nRwXx8aSirCSemOQp1_'
+        GoogleDriveId: '1KZpfS0_k0zuF1nRwXx8aSirCSemOQp1_',
+        DistanceFromMiddleC: -1
     }
 }
+
+export const noteSoundsArr: INoteSound[] = Object.entries(noteSounds).map(([propName, noteSound]) => noteSound);
+
+const distancesFromMiddleC: number[] = noteSoundsArr.map((noteSound: INoteSound) => noteSound.DistanceFromMiddleC);
+
+export const MIN_DISTANCE_FROM_MIDDLE_C: number = Math.min(...distancesFromMiddleC);
+export const MAX_DISTANCE_FROM_MIDDLE_C: number = Math.max(...distancesFromMiddleC);
